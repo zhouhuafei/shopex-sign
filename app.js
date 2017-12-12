97036c38af4164e17c84e9abdd73dd9698723e3b
zhouhuafei <zhouhuafei@shopex.cn>
2017-12-12 10:37:12 +0800

    app

--- app.js
+++ app.js
@@ -6,6 +6,19 @@ const isProduction = env !== 'development'; // 是否是生产环境
 const express = require('express');
 const app = express();
 
+// gzip压缩
+const compression = require('compression');
+app.use(compression()); // gzip压缩
+
+// 模版引擎(ejs)
+const ejs = require('ejs');
+app.set('views', 'views');
+app.set('view engine', 'ejs');
+app.set('view cache', true);
+
+// 托管资源文件
+app.use(express.static('assets'));
+
 // 404
 app.use(function (req, res, next) {
     res.status(404).send('404 - not found');
