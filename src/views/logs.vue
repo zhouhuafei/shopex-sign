7449da86daf83a7980b279cc4a1520a097ed0b46
zhouhuafei <zhouhuafei@shopex.cn>
2017-12-15 12:32:54 +0800

    build

--- src/views/logs.vue
+++ src/views/logs.vue
@@ -21,7 +21,7 @@
         data() {
             return {
                 nowPage: 1,
-                nowCount: 2,
+                nowCount: 20,
                 resultData: [],
             };
         },
