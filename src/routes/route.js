71573a91572d077a4faa69b837b8f466c7b14ab2
zhouhuafei <1123486116@qq.com>
2017-12-24 14:05:03 +0800

    route

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import routeConfig from './config';
 
 Vue.use(Router);
 
@@ -21,7 +20,7 @@ const routes = [
     // 签到
     {
         path: '/',
-        name: 'signIn',
+        name: 'sign-in',
         meta: {
             title: '签到',
         },
@@ -34,7 +33,7 @@ const routes = [
     // 签退
     {
         path: '/sign-out/',
-        name: 'signOut',
+        name: 'sign-out',
         meta: {
             title: '签退',
         },
@@ -46,31 +45,9 @@ const routes = [
     },
 ];
 
-const routes2 = [];
-Object.keys(routeConfig).forEach(function (key) {
-    const v = routeConfig[key];
-    routes2.push({
-        path: v.pathRoute,
-        name: v.name,
-        meta: v.meta,
-        /*
-        component: function (resolve) {
-            require.ensure([], function () {
-                resolve(require(`${path.view}${v.component ? v.component : key}.vue`));
-            }, key);
-        },
-        */
-        // component: resolve => require([`${path.view}${v.component ? v.component : key}.vue`], resolve),
-        // component: require(pathView),
-        component: require('../views/logs.vue'), // 写成变量就是不行,呵呵呵,去路由页面添加试试
-    });
-});
-console.log(routes);
-console.log(routes2);
-
 export default new Router({
     // mode: 'history',
     // base: '/base/url/',
     // 上述模式需要后台配置一些什么东西,懒得看,先贴个链接:https://router.vuejs.org/zh-cn/essentials/history-mode.html
-    routes: routes2,
+    routes: routes,
 });
