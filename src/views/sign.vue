d6f0e78fbfc9b0f96580fb08f68e87611368d355
zhouhuafei <zhouhuafei@shopex.cn>
2017-12-25 13:22:32 +0800

    bg

--- src/views/sign.vue
+++ src/views/sign.vue
@@ -126,9 +126,9 @@
         margin-top: px2rem(20);
     }
 
-    .button-sign-out{
-        background: #fc4a1a;  /* fallback for old browsers */
-        background: -webkit-linear-gradient(to left, #f7b733, #fc4a1a);  /* Chrome 10-25, Safari 5.1-6 */
-        background: linear-gradient(to left, #f7b733, #fc4a1a); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+    .button-sign-out {
+        background: #fc4a1a; /* fallback for old browsers */
+        background: -webkit-linear-gradient(to right, #f7b733, #fc4a1a); /* Chrome 10-25, Safari 5.1-6 */
+        background: linear-gradient(to right, #f7b733, #fc4a1a); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
     }
 </style>
