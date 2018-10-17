2ffcbd45d34e1bc2e2e0de40fa3bdda14bb56ab1
zhouhuafei <1123486116@qq.com>
2018-10-17 22:31:24 +0800

    md

--- db/config.js
+++ db/config.js
@@ -15,12 +15,12 @@ module.exports = {
         development: {
             host: 'localhost',
             port: '6379',
-            db: 12,
+            db: 1,
         },
         production: {
             host: 'localhost',
             port: '6379',
-            db: 12,
+            db: 1,
         },
     },
 };
